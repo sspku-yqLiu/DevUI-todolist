a59807990c2040e815cb6f94dcccfbac26c9a8cb
sspku-yqLiu <56879942+sspku-yqLiu@users.noreply.github.com>
2019-12-02 21:04:43 +0800

    2019/12/2>>添加任务清单等三个组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import { DevUIModule } from 'ng-devui';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuadrantComponent } from './components/quadrant/quadrant.component';
+import { ListComponent } from './components/list/list.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuadrantComponent,
+    ListComponent,
+    AddTaskComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/quadrant/quadrant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quadrant',
+  templateUrl: './quadrant.component.html',
+  styleUrls: ['./quadrant.component.css']
+})
+export class QuadrantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
